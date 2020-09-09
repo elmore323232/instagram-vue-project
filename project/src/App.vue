18d5659343fc47a2bacf72552496f955d2c5bf6c
Levkin Evgeniy <evgenij.levkin@promo.ru>
2020-09-09 16:19:24 +0300

    add clicked menu

--- project/src/App.vue
+++ project/src/App.vue
@@ -8,22 +8,23 @@
 
                 <v-spacer></v-spacer>
 
+                <router-link class="pointer icon" tag="span" to="/feed">
+                  <v-icon>mdi-home</v-icon>
+                </router-link>
+
                 <v-btn icon>
-                  <v-icon>mdi-magnify</v-icon>
+                  <v-icon>mdi-send</v-icon>
                 </v-btn>
 
                 <v-btn icon>
                   <v-icon>mdi-heart</v-icon>
                 </v-btn>
 
-                <v-btn icon>
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-
-                
-                <v-btn icon>
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
+                <router-link tag="span" class="pointer" to="/user">
+                  <v-avatar size="30">
+                    <img src="https://media3.s-nbcnews.com/j/newscms/2019_41/3047866/191010-japan-stalker-mc-1121_06b4c20bbf96a51dc8663f334404a899.fit-760w.JPG" alt="">
+                  </v-avatar>
+                </router-link>
               </v-toolbar>
     </v-container>
 
@@ -44,3 +45,9 @@ export default {
 
 };
 </script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
