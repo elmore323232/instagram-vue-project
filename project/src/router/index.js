a42bf33f318f84e144484cae6d4ce72d636e8c15
Levkin Evgeniy <evgenij.levkin@promo.ru>
2020-09-09 13:41:58 +0300

    routes and components

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../components/auth/Login'
+import Registration from '../components/auth/Registration'
+import User from '../components/User'
+import Feed from '../components/Feed'
 
 Vue.use(VueRouter)
 
   const routes = [
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/registration',
+      component: Login,
+    },
+    {
+      path: '/User',
+      component: Login,
+    },
+    {
+      path: '/Feed',
+      component: Login,
+    },
 ]
 
 const router = new VueRouter({
